fcad9867ee6352d52f52c80fff43c3822f6cc26d
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-22 23:37:02 +0900

    Task_47 solved with functions

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -1,30 +1,40 @@
 ﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. */
 System.Console.Clear();
-
 string ToInputVar(string input_text)
 {
     Console.Write($"Введите {input_text} и нажмите Enter: ");
     string result = Console.ReadLine();
     return result;
 }
-
+float[,] ToFillTable(int n, int m)
+{
+    float[,] table = new float[n, m];
+    var rand = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        { table[i, j] = rand.NextSingle() * rand.Next(-100, 101); }
+    }
+    return table;
+}
+void ToPrintTable(float[,] table)
+{
+    int rows = table.GetUpperBound(0) + 1;    // количество строк
+    int columns = table.Length / rows;        // количество столбцов
+    for (int i = 0; i < rows; i++)
+    {
+        Console.Write($"{i + 1}-я:  ");
+        for (int j = 0; j < columns; j++)
+        {
+            string formating_result = string.Format("{0:f}", table[i, j]);//форматирование: оставить 2 знака после запятой
+            Console.Write($"|{formating_result}    ");
+        }
+        Console.WriteLine();
+    }
+}
 
 string input_text = "количество строк таблицы";
 int n = int.Parse(ToInputVar(input_text));
 input_text = "количество столбцов таблицы";
 int m = int.Parse(ToInputVar(input_text));
-
-float[,] table = new float[n, m];
-var rand = new Random();
-for (int i = 0; i < n; i++)
-{
-    Console.Write($"{i+1}-я:  ");
-    for (int j = 0; j < m; j++)
-    {
-
-        table[i, j] = rand.NextSingle() * rand.Next(-100, 101);
-        string formating_result = string.Format("{0:f}",table[i,j]);
-        Console.Write($"{formating_result} ");
-    }
-    Console.WriteLine();
-}
+ToPrintTable(ToFillTable(n, m));
